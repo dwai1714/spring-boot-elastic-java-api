f557b77048d3506a38230de50aa94185dbc176f0
Dwaipayan Chowdhury <dwai1714@DC-Mac.local>
2018-01-30 20:53:25 +0530

    used the attributes Map

--- src/test/java/com/dc/elastic/ElasticTests.java
+++ src/test/java/com/dc/elastic/ElasticTests.java
@@ -1,10 +1,17 @@
 package com.dc.elastic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.lucene.search.join.ScoreMode;
+import org.elasticsearch.action.admin.indices.mapping.get.GetFieldMappingsResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
@@ -24,9 +31,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ElasticTests {
@@ -36,63 +40,99 @@ public class ElasticTests {
 
 	@Test
 	public void doBasicTest() {
+		SearchRequestBuilder requestAttOrderBuilder = client.prepareSearch("my_ord_index").setTypes("attOrder");
+		QueryBuilder attQB = QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("Type", "Fans"));
+		requestAttOrderBuilder.setQuery(attQB);
+		SearchResponse attResponse = requestAttOrderBuilder.get();
+		SearchHit[] hits = attResponse.getHits().getHits();
+		Map<String, Object> attSource = hits[0].getSourceAsMap();
+		Map<String, Integer> order = (Map<String, Integer>) attSource.get("order");
+
+		Set<Entry<String, Integer>> set = order.entrySet();
+		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				return (o1.getValue()).compareTo(o2.getValue());// Ascending order
+				// return (o2.getValue()).compareTo( o1.getValue() );//Descending order
+			}
+		});
+
 		logger.info("Preparing query");
 		SearchRequestBuilder requestBuilder = client.prepareSearch("my_index").setTypes("products");
 		AggregationBuilder aggregation = AggregationBuilders.nested("all_attributes", "attributes");
-		
-		aggregation.subAggregation(AggregationBuilders.terms("popular_Fan_Size").field("attributes.Fan Size"));		
-		aggregation.subAggregation(AggregationBuilders.terms("popular_Fan_Color").field("attributes.Fan Color.keyword"));
-		aggregation.subAggregation(AggregationBuilders.terms("popular_Fan_Display").field("attributes.Display Technology.keyword"));
-		
+
+		for (Map.Entry<String, Integer> entry : list) {
+
+			String termString = "popular_" + entry.getValue();
+
+			if (getDataType("attributes", entry.getKey()).equals("text"))
+				aggregation.subAggregation(AggregationBuilders.terms("popular_" + entry.getValue())
+						.field("attributes." + entry.getKey() + ".keyword"));
+			else
+				aggregation.subAggregation(
+						AggregationBuilders.terms("popular_" + entry.getValue()).field("attributes." + entry.getKey()));
+			// System.out.println(entry.getKey() + " ==== " + entry.getValue());
+			System.out.println("Term String  ==== " + termString);
+		}
 
 		NestedQueryBuilder nqb = QueryBuilders.nestedQuery("attributes",
-				QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("attributes.Brand", "LG")).
-				should(QueryBuilders.matchQuery("attributes.Brand", "Orient")),
-				ScoreMode.Max);
-		QueryBuilder qb = QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("Type", "TV's Refrigerator")).must(nqb);
-		
+				QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("attributes.Brand", "Orient")), ScoreMode.Max);
+		QueryBuilder qb = QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("Type", "Fans")).must(nqb);
 
 		requestBuilder.setQuery(qb);
-		//requestBuilder.setQuery(nqb);
+		// requestBuilder.setQuery(nqb);
 		requestBuilder.addAggregation(aggregation);
 
 		// Get response
 		logger.info("Executing query");
 		SearchResponse response = requestBuilder.get();
 
-		// Terms terms = response.getAggregations().get("all_attributes");
 		Nested agg = response.getAggregations().get("all_attributes");
-		Terms fanSize = agg.getAggregations().get("popular_Fan_Size");
-		Terms fanColor = agg.getAggregations().get("popular_Fan_Display");
-	
-		SearchHit[] hits = response.getHits().getHits();
+
+		for (Map.Entry<String, Integer> entry : list) {
+			Terms terms = agg.getAggregations().get("popular_" + entry.getValue());
+			terms.getBuckets().forEach(bucket -> {
+				String keyString = bucket.getKeyAsString();
+				long docCount = bucket.getDocCount();
+				logger.info("KeyString " + entry.getKey() + " is " + keyString + " docCount is " + docCount);
+
+			});
+
+			// System.out.println(entry.getKey() + " ==== " + entry.getValue());
+		}
+
+		hits = response.getHits().getHits();
 		Arrays.asList(hits).forEach(hit -> {
-			Map<String, Object> source = hit.getSourceAsMap(); 
-			Object object = source.get("attributes");
-			logger.info("Source Object is " + source); // Source Object
-		//	logger.info("Attributes are " + object); // Attributes
-			// score when the score mode is the default(average)
+			Map<String, Object> sourceObject = hit.getSourceAsMap();
+			Object attributesObject = sourceObject.get("attributes");
+			logger.info("Source Object is " + sourceObject); // Source Object
 			logger.info("Score by average: " + hit.getScore());
 		});
 
-		//Fan Size Bucket - Just want the key and value
-		fanSize.getBuckets().forEach(bucket -> {
-	        String keyString = bucket.getKeyAsString();
-	        long docCount = bucket.getDocCount();
-	        logger.info("Color KeyString is " + keyString + " docCount is " + docCount);
-
-		});	
-		
-		//Fan Color Bucket - Just want the key and value
-
-		fanColor.getBuckets().forEach(bucket -> {
-	        String keyString = bucket.getKeyAsString();
-	        long docCount = bucket.getDocCount();
-	        logger.info("Size KeyString is " + keyString + " docCount is " + docCount);
-
-		});		
-		
 		logger.info("agg.getDocCount() is " + agg.getDocCount());
+	}
+
+	public String getDataType(String nestedField, String field) {
+
+		GetFieldMappingsResponse response = client.admin().indices().prepareGetFieldMappings().addIndices("my_index")
+				.addTypes("products").setFields(nestedField + "." + field).get();
+
+		Map<String, Object> fieldSource;
+		try {
+			fieldSource = response.fieldMappings("my_index", "products", nestedField + "." + field).sourceAsMap();
+		} catch (Exception e) {
+			// Bad Code but will figure later
+			return "text";
+		}
+
+		String typeIs = (String) ((LinkedHashMap) (fieldSource.get(field))).get("type");
+		return typeIs;
+
+	}
+
+	@Test
+	public void doTypeTest() {
+		getDataType("attributes", "Fan Feature");
 	}
 
 }
